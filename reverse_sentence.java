d16542bd10700a8b1b1bc88ac4d6b20fa5847841
choodes <choodeswaran@gmail.com>
2026-08-14 19:00:49 +0530

    Create reverse_sentence.java

---
+++ reverse_sentence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String reverseWords(String s) {
+        String words[]=s.split(" ");
+        int l=0,r=words.length-1;
+        while(l<r){
+           String temp= words[l];
+            words[l]=words[r];
+            words[r]=temp;
+            l++;
+            r--;
+        }
+        StringBuilder result=new StringBuilder();
+        for(String word:words){
+            if(!word.isEmpty()){
+                if(result.length()>0){
+                    result.append(" ");
+                }
+            
+            result.append(word);
+            }
+        }
+        return result.toString();
+    }
+}
