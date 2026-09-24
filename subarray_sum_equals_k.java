0b0b0d3d2ac4e512d8bce93b491a4c33a00b5797
Choodeswaran D <choodeswaran@gmail.com>
2026-09-24 20:22:42 +0530

    Create subarray_sum_equals_k.java

---
+++ subarray_sum_equals_k.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int sum=0;
+        int c=0;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        map.put(0,1);
+       for(int i:nums){
+        sum+=i;
+        if(map.containsKey(sum-k)){
+           c+=map.get(sum-k);
+        }
+         map.put(sum,map.getOrDefault(sum,0)+1);
+       }
+       return c;
+    }
+}
